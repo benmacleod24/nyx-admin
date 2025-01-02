ce51864490f275c2d35ca45afddc74dceab69aa2
Ben Macleod <benstreasurebox@gmail.com>
2025-01-02 09:37:52 -0500

    dashboard stuff

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dc9aeb3b5ce0d7a3204531c5242838130e3678c")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/src/components/layouts/Dashboard/index.tsx
+++ frontend/src/components/layouts/Dashboard/index.tsx
@@ -1,5 +1,12 @@
+import { Sidebar, SidebarProvider } from "@/components/ui/sidebar";
 import React from "react";
+import AppSidebar from "./side-bar";
 
 export default function DashboardLayout(props: React.PropsWithChildren) {
-	return <div>Dashboard Layout{props.children}</div>;
+	return (
+		<SidebarProvider>
+			<AppSidebar />
+			<main>{props.children}</main>
+		</SidebarProvider>
+	);
 }
